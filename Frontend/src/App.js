1d5d28eb2095b21f999a4f1dac59604b3ae70e8a
Gaurav370 <gaurav.sekhri@excavateresearch.com>
2022-06-03 15:13:25 +0530

    updated2

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DetailsPage from './Components/DetailsPage';
+
+function App() {
+  return (
+    <div className="App">
+      <DetailsPage/>
+    </div>
+  );
+}
+
+export default App;
